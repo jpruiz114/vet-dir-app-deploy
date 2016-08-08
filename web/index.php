d478843f029487c975268039e776875ad76fb956
Jean Paul Ruiz Vallejo <jeanpaul-vp@pet360.com>
2016-08-08 10:32:51 -0500

    Adding index default page

---
+++ web/index.php
@@ -0,0 +1,15 @@
+<html>
+	<head>
+		<title>giphy.js demo</title>
+		
+		<script src="giphy.js"></script>
+	</head>
+	
+	<body>
+		<center>
+			<p>Seems like you are kinda lost, so here's a gif of a cat:<p>
+			
+			<span id = "giphyme"></span>
+		</center>
+	</body>
+</html>
